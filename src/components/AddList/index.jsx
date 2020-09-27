cf6a7dc24364c00857c87065a8c67ec093c453ad
Roman Prokopenko <mr.roman1201@gmail.com>
2020-09-27 19:43:07 +0300

    Adding tasks

---
+++ src/components/AddList/index.jsx
@@ -0,0 +1,124 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import List from '../List';
+import Badge from '../Badge';
+
+import closeSvg from '../../assets/img/close.svg';
+
+import './AddList.scss';
+
+const AddList = ({ colors, onAdd }) => {
+  const [visiblePopup, setVisiblePopup] = useState(false);
+  const [seletedColor, selectColor] = useState(3);
+  const [isLoading, setIsLoading] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    if (Array.isArray(colors)) {
+      selectColor(colors[0].id);
+    }
+  }, [colors]);
+
+  const onClose = () => {
+    setVisiblePopup(false);
+    setInputValue('');
+    selectColor(colors[0].id);
+  };
+
+  const addList = () => {
+    if (!inputValue) {
+      alert('Enter list name');
+      return;
+    }
+    setIsLoading(true);
+    axios
+      .post('http://localhost:3001/lists', {
+        name: inputValue,
+        colorId: seletedColor
+      })
+      .then(({ data }) => {
+        const color = colors.filter(c => c.id === seletedColor)[0].name;
+        const listObj = { ...data, color: { name: color } };
+        onAdd(listObj);
+        onClose();
+      }).catch(() => {
+        alert('Error: An error occurred while adding list!')
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <div className="add-list">
+      <List
+        onClick={() => setVisiblePopup(true)}
+        items={[
+          {
+            className: 'list__add-button',
+            icon: (
+              <svg
+                width="12"
+                height="12"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M8 1V15"
+                  stroke="black"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M1 8H15"
+                  stroke="black"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            ),
+            name: 'Add list'
+          }
+        ]}
+      />
+      {visiblePopup && (
+        <div className="add-list__popup">
+          <img
+            onClick={onClose}
+            src={closeSvg}
+            alt="Close button"
+            className="add-list__popup-close-btn"
+          />
+
+          <input
+            value={inputValue}
+            onChange={e => setInputValue(e.target.value)}
+            className="field"
+            type="text"
+            placeholder="Name list"
+          />
+
+          <div className="add-list__popup-colors">
+            {colors.map(color => (
+              <Badge
+                onClick={() => selectColor(color.id)}
+                key={color.id}
+                color={color.name}
+                className={seletedColor === color.id && 'active'}
+              />
+            ))}
+          </div>
+          <button onClick={addList} className="button">
+            {isLoading ? 'Adding...' : 'Add'}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AddList;
---
+++ src/components/Tasks/AddTaskForm.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+
+import addSvg from '../../assets/img/add.svg';
+
+import './Tasks.scss';
+
+const AddTaskForm = ({ list, onAddTask }) => {
+
+    const [visibleForm, setVisibleForm] = useState(false);
+    const [inputValue, setinputValue] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const toggleFormVisible = () => {
+        setVisibleForm(!visibleForm);
+        setinputValue('');
+    }
+
+    const addTask = () => {
+        const obj = {
+            'listId': list.id,
+            'text': inputValue,
+            'completed': false
+        }
+        setIsLoading(true);
+        axios.post('http://localhost:3001/tasks', obj).then(({ data }) => {
+            onAddTask(list.id, data)
+            toggleFormVisible();
+        }).catch(() => {
+            alert('Error: An error occurred while adding tasks!')
+        }).finally(() => {
+            setIsLoading(false)
+        });
+    }
+
+    return (
+        <div className='tasks__form'>
+            {!visibleForm ?
+                (<div onClick={toggleFormVisible} className='tasks__form-new'>
+                    <img src={addSvg} alt='Add icon' />
+                    <span>New task</span>
+                </div>) :
+                (<div className='tasks__form-block'>
+                    <input value={inputValue} className='field' type='text' placeholder='e.g.  Go to the store' onChange={e => { setinputValue(e.target.value) }} />
+                    <button disabled={isLoading} onClick={addTask} className='button'>
+                        {isLoading ? 'Adding...' : 'Add task'}
+                    </button>
+                    <button onClick={toggleFormVisible} className='button button--grey'>Cancel</button>
+                </div>)}
+        </div >
+    )
+}
+
+export default AddTaskForm
